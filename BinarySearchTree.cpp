12c04a2e3f5115c4f29cfbc6756ee1c27144c761
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-06-26 10:01:52 +0700

    membuat variabel info untuk menyimpan data dan pointer left child, right child dengan akses public

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 class Node
 {
-
+public:
+    int info; // membuat variabel info untuk menyimpan data
+    Node *leftchild; // Pointer to the left child
+    Node *rightchild; // Pointer to the right child
 };
 
