0f761e61a576e1665359b148f5205f09716cff9d
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-06-26 10:31:37 +0700

    inisialisasi left dan right child dari newnode  ke null

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -39,7 +39,12 @@ public:
         // Step 2: Assign value to the data field of new node
         newNode->info = x;
 
+        // Step 3: Make the left and right child of the new node point ot NULL
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
         
+    }
 }
 
  
